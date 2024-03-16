a08f569d8d851b657b2bec3b372b148560062ee6
GalileiGalileo <galianogalianei@gmail.com>
2024-03-16 19:47:54 +0100

    Added basic functionallity

--- main.c
+++ main.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  if (argc >= 4) {
-    printf("%d %s %s %s\n", argc, argv[1], argv[2], argv[3]);
+  if (argc >= 2) {
+    printf("Main logic");
+    FILE *file;
+    char file_ch;
+
+    file = fopen(argv[1], "r");
+    if (NULL == file) {
+      printf("File can't be opened\n");
+    }
+
+    printf("File contents: ");
+
+    do {
+      file_ch = fgetc(file);
+      printf("%c", file_ch);
+    } while (file_ch != EOF);
+
+    fclose(file);
   } else {
-    printf("Insufficient arguments provided.\n");
+    char a[256];
+    while (scanf("%s", a) != EOF) {
+      printf("%s\n", a);
+    }
   }
 
   return 0;
