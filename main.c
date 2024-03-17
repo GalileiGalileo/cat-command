0d66c8dd28669998ea9deade1d0b07edf1966557
GalileiGalileo <galianogalianei@gmail.com>
2024-03-17 20:53:46 +0100

    Changed way of getting flags from users

--- main.c
+++ main.c
@@ -3,54 +3,81 @@
 
 // TODO add -T, -E, strcmp custom function
 
+typedef struct Options {
+  int n;
+  int T;
+  int E;
+} Options;
+
+Options get_user_options(int argc, char *argv[], Options *options);
+void print_file(FILE *file, Options *options);
+
 int main(int argc, char *argv[]) {
+  Options options = {0, 0, 0};
+  options = get_user_options(argc, argv, &options);
+
   if (argc >= 2) {
     FILE *file;
-    char file_ch;
 
-    int line_numbers = 0;
     for (int i = 1; i < argc; i++) {
-      if (argv[i][0] == '-' && argv[i][1] == 'n' && argv[i][2] == '\0') {
-        line_numbers = 1;
-      } else {
-        file = fopen(argv[i], "r");
-        if (file != NULL) {
-          break;
-        }
+      file = fopen(argv[i], "r");
+      if (file != NULL) {
+        break;
       }
     }
 
     if (file == NULL) {
       fprintf(stderr, "Your file could not be opened!\n");
-      return -1;
+      return EXIT_FAILURE;
     }
 
-    int new_lines = 0;
-    int within_line = 0;
-    do {
-      file_ch = fgetc(file);
-
-      if (file_ch == '\n') {
-        within_line = 0;
-        new_lines++;
-        if (line_numbers == 1) {
-          printf("\r\t%i\t", new_lines);
-        }
-      } else if (!within_line) {
-        within_line = 1;
-        if (line_numbers == 1) {
-          printf("\r\t%i\t", new_lines);
-        }
-      }
-
-      printf("%c", file_ch);
-    } while (file_ch != EOF);
-    fclose(file);
+    print_file(file, &options);
   } else {
+
     char a;
     while ((a = getchar()) != EOF) {
       printf("%c", a);
     }
   }
-  return 0;
+  return EXIT_SUCCESS;
+}
+
+Options get_user_options(int argc, char *argv[], Options *options) {
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] == '-' && argv[i][1] == 'n' && argv[i][2] == '\0') {
+      options->n = 1;
+    } else if (argv[i][0] == '-' && argv[i][1] == 'E' && argv[i][2] == '\0') {
+      options->E = 1;
+    } else if (argv[i][0] == '-' && argv[i][1] == 'T' && argv[i][2] == '\0') {
+      options->T = 1;
+    }
+  }
+
+  return *options;
+};
+
+void print_file(FILE *file, Options *options) {
+  char file_ch;
+
+  int new_lines = 0;
+  int within_line = 0;
+  do {
+    file_ch = fgetc(file);
+
+    if (file_ch == '\n') {
+      within_line = 0;
+      new_lines++;
+      if (options->n == 1) {
+        printf("\r\t%i\t", new_lines);
+      }
+    } else if (!within_line) {
+      within_line = 1;
+      if (options->n == 1) {
+        printf("\r\t%i\t", new_lines);
+      }
+    }
+
+    printf("%c", file_ch);
+  } while (file_ch != EOF);
+  fclose(file);
 }
